afc89f4f19cd70bab8720ed38a7f2e81a20149b2
Informatica <informatica@alumno.local>
2024-08-02 17:18:40 -0600

    Validaciones

--- src/main/java/com/cristianlima/webapp/biblioteca/model/Categoria.java
+++ src/main/java/com/cristianlima/webapp/biblioteca/model/Categoria.java
@@ -1,12 +1,12 @@
 package com.cristianlima.webapp.biblioteca.model;
 
-import org.antlr.v4.runtime.misc.NotNull;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.ToString;
 
@@ -18,5 +18,6 @@ public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull(message = "NombreCategoria no puede ser nulo")
     private String nombreCategoria;
 }
