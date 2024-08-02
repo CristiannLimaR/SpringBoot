0a3f30d3566abf2bca443a7d66a72bcff4f27742
Informatica <informatica@alumno.local>
2024-08-02 16:18:55 -0600

    Editar y Eliminar

--- src/main/java/com/cristianlima/webapp/biblioteca/controller/CategoriaController.java
+++ src/main/java/com/cristianlima/webapp/biblioteca/controller/CategoriaController.java
@@ -13,10 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cristianlima.webapp.biblioteca.model.Categoria;
 import com.cristianlima.webapp.biblioteca.service.CategoriaService;
 
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @Controller
 @RestController
@@ -32,6 +35,15 @@ public class CategoriaController {
         return categoriaService.listarCategorias();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Categoria> buscarCategoriaPorId(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(categoriaService.buscarCategoriaPorId(id));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
     @PostMapping("/")
     public ResponseEntity<Map<String, String>> agregarCategoria(@RequestBody Categoria categoria) {
         Map<String, String> response = new HashMap<>();
@@ -43,6 +55,38 @@ public class CategoriaController {
             response.put("err", "Error al agregar la categoria");
             return ResponseEntity.badRequest().body(response);
         }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Map<String, String>> editarCategoria(@PathVariable Long id,
+            @RequestBody Categoria newCategoria) {
+        Map<String, String> response = new HashMap<>();
+        try {
+            Categoria oldCategoria = categoriaService.buscarCategoriaPorId(id);
+            oldCategoria.setNombreCategoria(newCategoria.getNombreCategoria());
+            categoriaService.guardarCategoria(oldCategoria);
+            response.put("message","La categoria se edito con éxito");
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            response.put("err","La categoria no se pudo editar");
+            return ResponseEntity.ok(response);
+        }
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Map<String, String>> eliminarCategoria(@PathVariable Long id) {
+        Map<String, String> response = new HashMap<>();
+        try {
+            Categoria categoria = categoriaService.buscarCategoriaPorId(id);
+            categoriaService.eliminarCategoria(categoria);
+            response.put("message", "eliminado con éxito");
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            response.put("err", "No se pudo eliminar la categoria");
+            return ResponseEntity.badRequest().body(response);
+        }
+
     }
 
 }
--- src/main/java/com/cristianlima/webapp/biblioteca/model/Categoria.java
+++ src/main/java/com/cristianlima/webapp/biblioteca/model/Categoria.java
@@ -1,5 +1,7 @@
 package com.cristianlima.webapp.biblioteca.model;
 
+import org.antlr.v4.runtime.misc.NotNull;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
